87355b24e4f3440b0760bff37ca809c58084effd
Pedro Filipetto <pedrofilipetto@gmail.com>
2025-08-21 15:08:19 -0300

    Corrigindo os exercicios

--- ListaExercicios2/Exercicio001.c
+++ ListaExercicios2/Exercicio001.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
-/*Escreva uma funcao em C para um caixa de banco, que recebe um valor inteiro r (maior ou igual a zero) 
+/*
+ * Arquivo: Exercicio001 - Lista 2
+ * Autor: Pedro Castiglia Filipetto
+ 
+Escreva uma funcao em C para um caixa de banco, que recebe um valor inteiro r (maior ou igual a zero) 
 e determina o numero de notas de 100, 50, 10, 5 e 1 reais necessario para pagar a quantia r. 
 Faca de forma que o numero ´de notas usado seja o menor poss´ıvel, retornando as quantidades de notas por referencia.
 */
+void papelMoeda(
+    unsigned r, 
+    unsigned *n100, 
+    unsigned *n50, 
+    unsigned *n10, 
+    unsigned *n5, 
+    unsigned *n1
+);
+
 int main() {
     unsigned r, n100, n50, n10, n5, n1;
-
     scanf("%u", &r);
-
     papelMoeda(r, &n100, &n50, &n10, &n5, &n1);
-    printf("%u %u %u %u %u", n100, n50, n10, n5, n1);
-
+    printf("%u %u %u %u %u\n", n100, n50, n10, n5, n1);
     return 0;
 }
 
-void papelMoeda(unsigned r, unsigned *n100, unsigned *n50, unsigned *n10, unsigned *n5, unsigned *n1){
-    
-    *n100 = r / 100;
-	r = r % 100;
+void papelMoeda(
+    unsigned r, 
+    unsigned *n100, 
+    unsigned *n50, 
+    unsigned *n10, 
+    unsigned *n5, 
+    unsigned *n1
+) {
+    unsigned restante = r;
+    unsigned notasVal[] = {100, 50, 10, 5, 1};
+    unsigned *notas[] = {n100, n50, n10, n5, n1};
 
-    *n50 = r / 50;
-	r = r % 50;
-
-    *n10 = r / 10;
-	r = r % 10;
-
-    *n5 = r / 5;
-	r = r % 5;
-
-    *n1 = r;
+    for (int i = 0; i < 5; i++) {
+        *(notas[i]) = restante / notasVal[i];
+        restante %= notasVal[i];
+    }
 }
--- ListaExercicios2/Exercicio002.c
+++ ListaExercicios2/Exercicio002.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-
-int main(unsigned seg, unsigned *h, unsigned *m, unsigned *s) {
-    unsigned seg, h, m, s;
-    scanf("%u", &seg);
-    hms(seg, &h, &m, &s);
-    printf("%u %u %u\n", h, m, s);
-    return 0;    
-}
-
+/*
+Arquivo: Exercicio002 - Lista 2
+Autor: Pedro Castiglia Filipetto
+*/
 void hms(unsigned seg, unsigned *h, unsigned *m, unsigned *s);
-    int main() {
+
+int main() {
     unsigned seg, h, m, s;
     scanf("%u", &seg);
     hms(seg, &h, &m, &s);
     printf("%u %u %u\n", h, m, s);
     return 0;
+}
+
+void hms(unsigned seg, unsigned *h, unsigned *m, unsigned *s) {
+    *h = seg / 3600;
+    seg -= *h*3600;
+    *m = seg / 60;
+    seg -= *m*60;
+    *s = seg;
 }
