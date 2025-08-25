89017ecb9578bc836777700d84084a0965ec638a
Pedro Filipetto <pedrofilipetto@gmail.com>
2025-08-25 15:36:32 -0300

    Add initial implementation for Exercicio003 and Exercicio004 in ListaExercicos3

---
+++ src/ListaExercicos3/Exercicio003.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+/*
+Considere uma linha no formato CSV (Comma-Separated Values) que use como separador o caractere ’;’ (ponto-virgula) 
+e que contenha as seguintes informacoes sobre uma pessoa (exatamente na seguinte ordem): nome (ate 40 caracteres uteis), 
+idade (expressa como um valor inteiro), peso (em kg, expresso como um numero real, usando ponto como separador de casas decimais) 
+e altura (em metros, expressa como um numero real, usando ponto como separador de casas decimais). 
+Escreva um programa em C que leia uma linha nesse formato CSV, calculando o IMC (Indice de Massa Corporal) dessa Pessoa, 
+usando a seguinte formula:
+IMC = Peso / Altura * Altura
+Mostre o resultado no seguinte formato: nome da pessoa, texto “: ” e IMC dessa pessoa (com 4 casas decimais).
+*/
+
